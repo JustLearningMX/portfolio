85e7bb30d3d37e45541c10adbc1693757642c494
Hiram Chavez <virtual.liga@gmail.com>
2023-05-09 12:04:27 -0600

    Actualizando Presentacion y Links de proyectos

--- source/utils/formacion.js
+++ source/utils/formacion.js
@@ -2,15 +2,23 @@ export const estudios = [
     {
         'nombre': 'Universidad UNADM',
         'carrera': 'Ing. Software',
-        'periodo': 'Abril 2018 - Diciembre 2022',
+        'periodo': 'Abril 2018 - Enero 2023',
         'iconAwesome': null,
         'img': 'https://www.gob.mx/cms/uploads/article/main_image/55931/unadm.jpg',
         'alt': 'Logo de la universidad UNADM'
     },
     {
+        'nombre': 'Bedu',
+        'carrera': 'Desarrollo Web con Java',
+        'periodo': 'Noviembre 2022 - Julio 2023',
+        'iconAwesome': null,
+        'img': 'https://i2.wp.com/curiotek.com/wp-content/uploads/2017/06/Spring-Logo.png?w=851&ssl=1',
+        'alt': 'Logo del instituto Bedu'
+    },
+    {
         'nombre': 'Oracle - Alura',
         'carrera': 'Desarrollo Web Fullstack Java',
-        'periodo': 'Marzo - Octubre 2022',
+        'periodo': 'Marzo - Marzo 2023',
         'iconAwesome': null,
         'img': 'https://www.alura.com.br/assets/img/challenges/oracle-one/share_image.1647533644.jpg',
         'alt': 'Logo de Oracle e instituto Alura'
